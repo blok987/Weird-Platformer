fc08c89875e62fe2941a023c29e81857acea0d34
MUKILTEO\1726358 <bloked987@gmail.com>
2026-01-20 09:52:38 -0800

    Worked on Damage sensing. NOTES: study LERP, and rework movement.

--- Assets/Scripts/DmgAtkScript.cs
+++ Assets/Scripts/DmgAtkScript.cs
@@ -5,6 +5,7 @@ public class DmgAtkScript : MonoBehaviour
     public bool hasWeapon;
     public float atkDmg;
     public int atkSpd;
+    public Vector3 WeaponType;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -15,5 +16,9 @@ public class DmgAtkScript : MonoBehaviour
     void Update()
     {
         
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireCube(transform.position, WeaponType);
     }
 }
