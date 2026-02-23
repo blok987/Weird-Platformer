2f90796bdb667df476e21b68b43a67565a23818e
MUKILTEO\1726358 <bloked987@gmail.com>
2026-02-23 10:15:49 -0800

    Worked on dmg script and box scripts.

--- Assets/Scripts/DmgAtkScript.cs
+++ Assets/Scripts/DmgAtkScript.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class DmgAtkScript : MonoBehaviour
 {
@@ -7,28 +8,36 @@ public class DmgAtkScript : MonoBehaviour
     public int AtkSpd;
     public Vector3 WeaponType;
     public bool DmgGive;
+    public LayerMask Enemylayer;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+    public void Attack(InputAction.CallbackContext ctx)
+    {
+        if (ctx.performed)
+        {
+            Debug.Log("attack");
+
+            if (Physics.BoxCast(transform.position, WeaponType, Vector3.up, out RaycastHit hitinfo, Quaternion.identity, 1, Enemylayer))
+            {
+                //hitinfo.collider.gameObject
+                Debug.Log("Object Hit:" + hitinfo.collider.name);
+            }
+
+
+        }
+
     }
     private void OnDrawGizmos()
     {
         Gizmos.DrawWireCube(transform.position, WeaponType);
     }
-    private void OnCollisionEnter(Collision collision)
-    {
-       
-    }
-    private void OnCollisionStay(Collision collison)
-    {
-        Debug.Log("Hit");
-    }
-
 }
