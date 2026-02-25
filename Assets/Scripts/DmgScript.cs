7c9f08d324473d84c7cebeb9706606d5bdbb2d9f
MUKILTEO\1726358 <bloked987@gmail.com>
2026-02-25 10:04:44 -0800

    Revert "Began working on dummy and rock models"
    This reverts commit 097c0b1eff9595af9738772f803a7cbbed303733.

--- Assets/Scripts/DmgScript.cs
+++ Assets/Scripts/DmgScript.cs
@@ -8,14 +8,13 @@ public class DmgScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        
     }
-}
   
   
