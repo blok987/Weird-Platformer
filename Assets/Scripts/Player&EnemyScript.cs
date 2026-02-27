fe8013ed12230138835aa32efc32cbf7181a302b
MUKILTEO\1726358 <bloked987@gmail.com>
2026-02-27 09:33:29 -0800

    Worked on stats script. Begun working on death for enemies.

--- Assets/Scripts/Player&EnemyScript.cs
+++ Assets/Scripts/Player&EnemyScript.cs
@@ -1,9 +1,11 @@
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class StatsScript : MonoBehaviour
 {
     //Character Stats
-    public float StatsHealth;
+    public int StatsHealthLvL;
     //MIN-MAX Health Stats
     public float MaxHealth;
     public bool AtMaxHealth;
@@ -11,16 +13,30 @@ public class StatsScript : MonoBehaviour
     //NPC-Or-Not Stats
     public bool IsPlayer;
     public bool NotPlayer;
-    
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void Start() 
     {
-        
+        if (IsPlayer == false)
+        {
+            Debug.Log("Not Player");
+        }
+
+        else
+        {
+            Debug.Log("Is Not Player");
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (IsPlayer == false && AtMinHealth == true);
+        {
+            
+        }
+    
     }
+    
 }
+
