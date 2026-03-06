8e825616015d3960c733792c3705069041cae08b
MUKILTEO\1726358 <bloked987@gmail.com>
2026-03-06 09:32:28 -0800

    Finish X-Y movement, angular/rotation movement (made movement dependent on camera angle) NOTES: Finish jumping ground checks

--- Assets/Scripts/WalkScript.cs
+++ Assets/Scripts/WalkScript.cs
@@ -1,53 +1,79 @@
+using Unity.Android.Gradle.Manifest;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.Rendering;
 
 public class WalkScript : MonoBehaviour
 {
+
     public CharacterController cc;
-    public float JumpHeight;
-    public float MoveSpeed;
-    public float SprintSpeed;
     public Camera cam;
-    private bool IsGrounded;
-    private bool IsSprint;
-    public int Sprint;
+    
+    public float JumpHeight;
+    public float MoveSpeed = 5;
+    public float SprintSpeed;
+
+    [SerializeField] private bool IsGrounded;
+    [SerializeField] private bool IsSprint;
+    [SerializeField] private int Sprint;
+    [SerializeField] private float gravity = -9.8f;
+
     private Vector3 Walk;
     private Vector3 DirectionInp;
+    private float YVelocity;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        YVelocity += gravity * Time.deltaTime;
+
+        Vector3 CameraRotation = Camera.main.transform.forward;
+        CameraRotation.y  = 0;
+        transform.forward = CameraRotation;
+
+        Vector3 Movement = default;
+
         #region Movement Inputs
         //Uses Character controller inputs to move
         if (Input.GetKey(KeyCode.W))
         {
-            cc.Move(transform.forward);
+            Movement += transform.forward.normalized * MoveSpeed;
+            
         }
         
         if (Input.GetKey(KeyCode.S))
         {
-            cc.Move(-transform.forward);
+            Movement += -transform.forward.normalized * MoveSpeed;
         }
 
         if (Input.GetKey(KeyCode.D))
         {
-            cc.Move(transform.right);
+            Movement += transform.right.normalized * MoveSpeed;
         }
 
         if (Input.GetKey(KeyCode.A))
         {
-            cc.Move(-transform.right);
+            Movement += -transform.right.normalized * MoveSpeed;
         }
-        if (Input.GetKey(KeyCode.Space))
+
+        Movement.y = 0;
+
+        if (Input.GetKeyDown(KeyCode.Space)&& IsGrounded)
         {
-            cc.Move(transform.up);
+            YVelocity = JumpHeight;
+            IsGrounded = false;
         }
+        IsGrounded = true;
+        cc.Move(Movement + Vector3.up * YVelocity * Time.deltaTime);
         #endregion
     }
 
